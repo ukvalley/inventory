8d77becc456085c644c9566c6aef0dc686a2292b
ashish0202 <ashishshinde.ukvalley@gmail.com>
2023-01-18 12:47:12 +0530

    csv

---
+++ resources/views/import.blade.php
@@ -0,0 +1,67 @@
+@include('common.header')
+
+<div class="content-wrapper">
+   <section class="content">
+               <div class="row">
+                  <!-- Form controls -->
+                  <div class="col-sm-12">
+                     <div class="panel panel-bd lobidrag">
+                        <div class="panel-heading">
+                           <div class="btn-group" id="buttonlist"> 
+                              <a class="btn btn-add "href="purchase_device"> 
+                              <i class="fa fa-list"></i>EXel 1 </a>  
+                           </div>
+
+                        </div>
+                        <div class="panel-body">
+                             <h3>Csv</h3>
+                             <form class="form-horizontal" method="POST" action="{{ route('import_parse') }}" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('csv_file') ? ' has-error' : '' }}">
+                                <label for="csv_file" class="col-md-4 control-label">CSV file to import</label>
+
+                                <div class="col-md-6">
+                                    <input id="csv_file" type="file" class="form-control" name="csv_file" required>
+
+                                    @if ($errors->has('csv_file'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('csv_file') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="header" checked> File contains header row?
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Parse CSV
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        </div>
+                      </div>
+                   </div>
+                </div>
+             </section>
+
+
+ <!-- Main content -->
+
+            </div>
+
+
+
+
+@include('common.footer')
